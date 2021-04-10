4ba5ea8202ea12606c01835e8c819b2fd03ebfe8
Nidhi Patel <75785912+nidhipatel1213@users.noreply.github.com>
2021-04-10 16:54:48 +0530

    Update Cards.jsx

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -10,14 +10,14 @@ const Cards = () => {
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                        <CardItem
-                           src='images/img-9.jpg'
+                           src='/images/img-9.jpg'
                            text='Explore the hidden waterfall deep 
                            inside the Amazon Jungle'
                            label='Adventure'
                            path='/services'
                        />
                        <CardItem
-                           src='images/img-2.jpg'
+                           src='/images/img-2.jpg'
                            text='Travel through the Islands of Bali in a 
                            private Cruise'
                            label='Luxury'
